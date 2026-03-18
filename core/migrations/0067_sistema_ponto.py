f20d247a29c8ed56cc369b316aaef040a4a75fed
Jeferson Brito <jeffersonbrito2455@gmail.com>
2026-03-17 22:26:41 -0300

    fix(migrations): renomear 0065 para 0067 para resolver conflito de historico

--- core/migrations/0067_sistema_ponto.py
+++ core/migrations/0067_sistema_ponto.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0064_alter_analystassignment_active_and_more'),
+        ('core', '0066_documentocolaborador'),
     ]
 
     operations = [
